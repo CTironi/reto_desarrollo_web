898f7dbc3dd13c226bda24d30e457e9954be1b4b
Cristian Tironi <ctironi@estudiante.ceibal.edu.uy>
2022-07-15 21:06:38 -0300

    reto completado

---
+++ Back-reto/src/main/java/com/sofka/Software/models/ListModel.java
@@ -0,0 +1,49 @@
+package com.sofka.Software.models;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "lista")
+public class ListModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @OneToMany(fetch = FetchType.EAGER,
+            targetEntity = ListTaskModel.class,
+            cascade = CascadeType.REMOVE,
+            mappedBy = "listaid")
+    @JsonBackReference
+    private List<ListTaskModel>ListTask = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ListTaskModel> getListTask() {
+        return ListTask;
+    }
+
+    public void setListTask(List<ListTaskModel> listTask) {
+        ListTask = listTask;
+    }
+}
---
+++ Back-reto/src/main/java/com/sofka/Software/repositories/ListRepository.java
@@ -0,0 +1,8 @@
+package com.sofka.Software.repositories;
+import com.sofka.Software.models.ListModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ListRepository extends JpaRepository<ListModel, Long > {
+}
